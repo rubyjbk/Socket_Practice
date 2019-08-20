da0478a44ad169affb1b7ed816b9a615cbdf3f2d
rubyjbk <emeraldjbk@hotmail.com>
2019-08-20 15:46:11 +0800

    formatted

--- 190820v1/SocketClient.java
+++ 190820v1/SocketClient.java
@@ -10,139 +10,128 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class SocketClient {
-	
-	private static String address="127.0.0.1";
-	private static int port=9876;
-	
-	public SocketClient(String cmd,String param) {
-		
-		if(cmd.equals("test")) {
+
+	private static String address = "127.0.0.1";
+	private static int port = 9876;
+
+	public SocketClient(String cmd, String param) {
+
+		if (cmd.equals("test")) {
 			function_test(param);
-		}
-		else if(cmd.equals("shutdown")) {
+		} else if (cmd.equals("shutdown")) {
 			function_shutdown();
-		}
-		else if(cmd.equals("shutdown")) {
+		} else if (cmd.equals("shutdown")) {
 			function_parseCSV(param);
 		}
-        
-		
+
 	}
-	
+
 	public void function_test(String param) {
 		Socket socket = null;
-        ObjectOutputStream oos = null;
-        ObjectInputStream ois = null;
-        int routine=Integer.parseInt(param);
-        
-        for(int i=0;i<routine;i++) {
-        	try {
-	            socket = new Socket(address, port);
-	            //write to socket using ObjectOutputStream
-	            oos = new ObjectOutputStream(socket.getOutputStream());
-	            System.out.println("*** Client Side: sending request to server");
-	            if(i==routine-1) {
-	            	oos.writeObject("shutdown");
-	            }
-	            else {
-	            	oos.writeObject(""+i);
-	            }
-	            
-	            
-	            //read the server response message
-	            ois = new ObjectInputStream(socket.getInputStream());
-	            String message = (String) ois.readObject();
-	            System.out.println("*** Client Side: got the respond message: " + message);
-	            //close resources
-	            ois.close();
-	            oos.close();
-	            socket.close();
-	            
-	            //Thread.sleep(100);
-	            
-        	}
-        	catch(IOException e) {
-        		System.out.println("IOException :" + e.toString());
-        	} catch (ClassNotFoundException e) {
+		ObjectOutputStream oos = null;
+		ObjectInputStream ois = null;
+		int routine = Integer.parseInt(param);
+
+		for (int i = 0; i < routine; i++) {
+			try {
+				socket = new Socket(address, port);
+				// write to socket using ObjectOutputStream
+				oos = new ObjectOutputStream(socket.getOutputStream());
+				System.out.println("*** Client Side: sending request to server");
+				if (i == routine - 1) {
+					oos.writeObject("shutdown");
+				} else {
+					oos.writeObject("" + i);
+				}
+
+				// read the server response message
+				ois = new ObjectInputStream(socket.getInputStream());
+				String message = (String) ois.readObject();
+				System.out.println("*** Client Side: got the respond message: " + message);
+				// close resources
+				ois.close();
+				oos.close();
+				socket.close();
+
+				// Thread.sleep(100);
+
+			} catch (IOException e) {
+				System.out.println("IOException :" + e.toString());
+			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        }
-        
+		}
+
 	}
-	
+
 	public void function_shutdown() {
 		Socket socket = null;
-        ObjectOutputStream oos = null;
-        ObjectInputStream ois = null;
-        
-        try {
-	        socket = new Socket(address, port);
-	        //write to socket using ObjectOutputStream
-	        oos = new ObjectOutputStream(socket.getOutputStream());
-	        System.out.println("*** Client Side: sending request to server");
-	        oos.writeObject("shutdown");
-	        
-	      //read the server response message
-            ois = new ObjectInputStream(socket.getInputStream());
-            String message = (String) ois.readObject();
-            System.out.println("*** Client Side: got the respond message: " + message);
-            //close resources
-            ois.close();
-	        
-	        oos.close();
-	        socket.close();
-        }
-        catch(Exception e) {
-        	System.out.println("*** Client Side: Exception :" + e.toString());
-        }
-        
-	}
-	
-	public void function_parseCSV(String param) {
-		String fileName=param;
-		
-		Socket socket = null;
-		FileInputStream fis = null;
-	    BufferedInputStream bis = null;
-	    OutputStream os = null;
-   
+		ObjectOutputStream oos = null;
+		ObjectInputStream ois = null;
+
 		try {
 			socket = new Socket(address, port);
-			File myFile = new File (fileName);
-	        byte [] mybytearray  = new byte [(int)myFile.length()];
-	        fis = new FileInputStream(myFile);
-	        bis = new BufferedInputStream(fis);
-	        bis.read(mybytearray,0,mybytearray.length);
-	       
-	        os = socket.getOutputStream();
-	        System.out.println("Sending " + fileName + "(" + mybytearray.length + " bytes)");
-	        os.write(mybytearray,0,mybytearray.length);
-	        os.flush();
-	        
-	        os.close();
-	        bis.close();
+			// write to socket using ObjectOutputStream
+			oos = new ObjectOutputStream(socket.getOutputStream());
+			System.out.println("*** Client Side: sending request to server");
+			oos.writeObject("shutdown");
+
+			// read the server response message
+			ois = new ObjectInputStream(socket.getInputStream());
+			String message = (String) ois.readObject();
+			System.out.println("*** Client Side: got the respond message: " + message);
+			// close resources
+			ois.close();
+
+			oos.close();
+			socket.close();
+		} catch (Exception e) {
+			System.out.println("*** Client Side: Exception :" + e.toString());
 		}
-		catch(Exception e) {
+
+	}
+
+	public void function_parseCSV(String param) {
+		String fileName = param;
+
+		Socket socket = null;
+		FileInputStream fis = null;
+		BufferedInputStream bis = null;
+		OutputStream os = null;
+
+		try {
+			socket = new Socket(address, port);
+			File myFile = new File(fileName);
+			byte[] mybytearray = new byte[(int) myFile.length()];
+			fis = new FileInputStream(myFile);
+			bis = new BufferedInputStream(fis);
+			bis.read(mybytearray, 0, mybytearray.length);
+
+			os = socket.getOutputStream();
+			System.out.println("Sending " + fileName + "(" + mybytearray.length + " bytes)");
+			os.write(mybytearray, 0, mybytearray.length);
+			os.flush();
+
+			os.close();
+			bis.close();
+		} catch (Exception e) {
 			System.out.println("IOException :" + e.toString());
 		}
-		
+
 	}
 
 	public static void main(String args[]) {
-		if(args.length==0) {
-			//default
-			new SocketClient("test","5");
-		}
-		else if(args.length==1) {
-			new SocketClient(args[0],"");
-		}
-		else if(args.length==2)
-			new SocketClient(args[0],args[1]);
+		if (args.length == 0) {
+			// default
+			new SocketClient("test", "5");
+		} else if (args.length == 1) {
+			new SocketClient(args[0], "");
+		} else if (args.length == 2)
+			new SocketClient(args[0], args[1]);
 		else {
 			System.out.println("");
 		}
 	}
-		
-}
 
+}
--- 190820v1/SocketServer.java
+++ 190820v1/SocketServer.java
@@ -6,60 +6,57 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class SocketServer extends java.lang.Thread{
-	
+public class SocketServer extends java.lang.Thread {
+
 	private static ServerSocket listerner;
-	
-	private static int port=9876;
-	
+
+	private static int port = 9876;
+
 	public SocketServer() {
-		try{
-			listerner=new ServerSocket(port);
-		}
-		catch(IOException e) {
+		try {
+			listerner = new ServerSocket(port);
+		} catch (IOException e) {
 			System.out.println("IOException :" + e.toString());
 		}
 	}
-	
+
 	public void run() {
-		while(true){
+		while (true) {
 			System.out.println("### Server Side: running and waiting for request");
-			
+
 			try {
-				Socket socket=listerner.accept();
-				
+				Socket socket = listerner.accept();
+
 				/*
-				 *  client -> server
+				 * client -> server
 				 */
 				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
 				String message = (String) ois.readObject();
-	            System.out.println("### Server Side: Message Received: " + message);
-	            
-	            /*
-				 *  server -> client
+				System.out.println("### Server Side: Message Received: " + message);
+
+				/*
+				 * server -> client
 				 */
-	            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-	            oos.writeObject("Respond from Server: "+message);
-	            
-	            ois.close();
-	            oos.close();
-	            socket.close();
-	            
-	            if(message.equalsIgnoreCase("shutdown")) {
-	            	System.out.println("### Server Side: got the CMD shutdown from client");
-	            	break;
-	            }
-	            
-			}catch(IOException e) {
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				oos.writeObject("Respond from Server: " + message);
+
+				ois.close();
+				oos.close();
+				socket.close();
+
+				if (message.equalsIgnoreCase("shutdown")) {
+					System.out.println("### Server Side: got the CMD shutdown from client");
+					break;
+				}
+
+			} catch (IOException e) {
 				System.out.println("### Server Side: IOException :" + e.toString());
 			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
-		
-		
+
 		try {
 			System.out.println("### Server Side: close socketserver");
 			listerner.close();
